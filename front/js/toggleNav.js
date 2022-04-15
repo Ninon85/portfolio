4fcc263c18296dbc1d54916c2ecca53450ca77a1
Ninon85 <88130318+Ninon85@users.noreply.github.com>
2022-04-15 08:14:42 +0200

    toggle navigation position fixed

--- front/js/toggleNav.js
+++ front/js/toggleNav.js
@@ -1,6 +1,5 @@
 const toggle = document.querySelector(".main-header__burger-container");
 const nav = document.querySelector(".main-header__nav");
-console.log(nav);
 toggle.addEventListener("click", () => {
 	nav.classList.toggle("toggle-nav");
 });
